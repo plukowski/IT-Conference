5a22033f6a60ec8799952a3b0a456e80fc5a0d8b
p.lukowski <p.lukowski0334@gmail.com>
2022-06-07 22:23:29 +0200

    -Corrected wrong implementation of counting stats per subject

--- src/main/java/com/plukowski/itconference/services/LectureService.java
+++ src/main/java/com/plukowski/itconference/services/LectureService.java
@@ -3,6 +3,8 @@ package com.plukowski.itconference.services;
 import com.plukowski.itconference.Schedule;
 import com.plukowski.itconference.exceptions.ExceptionWithMessage;
 import com.plukowski.itconference.models.Lecture;
+import com.plukowski.itconference.models.Participant;
+import com.plukowski.itconference.models.Reservation;
 import com.plukowski.itconference.repositories.LectureRepository;
 import com.plukowski.itconference.repositories.ParticipantRepository;
 import com.plukowski.itconference.repositories.ReservationRepository;
@@ -10,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.Part;
+import java.util.ArrayList;
 
 @Service
 public class LectureService {
@@ -45,12 +50,22 @@ public class LectureService {
             return result;
         }
         else if (type == 1){
+            ArrayList<Integer> participants = new ArrayList<>();
             for(int subject: Schedule.subjects.keySet()){
-                int participants = 0;
-                for(Lecture lecture: lectureRepository.findBySubjectId(subject)){
-                    participants += 5-lecture.getSlots();
+                participants.add(0);
+            }
+            for(Participant participant: participantRepository.findAll()){
+                ArrayList<Integer> alreadyCountedSubjects = new ArrayList<>();
+                for(Reservation reservation: reservationRepository.findByParticipantId(participant.getId())){
+                    int index = (lectureRepository.findById(reservation.getLectureId()).get().getSubjectId()) - 1;
+                    if(alreadyCountedSubjects.contains(index))
+                        continue;
+                    alreadyCountedSubjects.add(index);
+                    participants.set(index,participants.get(index)+1);
                 }
-                float interest = ((float) participants / (float) allParticipants * 100);
+            }
+            for(int subject: Schedule.subjects.keySet()){
+                float interest = ((float) participants.get(subject-1) / (float) allParticipants * 100);
                 result += "Subject="+Schedule.subjects.get(subject)+" interest="+interest+"%\n";
             }
             return result;
